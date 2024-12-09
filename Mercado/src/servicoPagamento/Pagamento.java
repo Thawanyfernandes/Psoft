0767383163b874362694654e332153f0512d4c88
Thawanny Fernandes <thawany.karoline.sousa.fernandes@ccc.ufcg.edu.br>
2024-12-09 06:24:24 -0300

    terminando versão 1

---
+++ Mercado/src/servicoPagamento/Pagamento.java
@@ -0,0 +1,13 @@
+package servicoPagamento;
+
+public class Pagamento implements InterfacePagamento{
+    
+        @Override
+        public double pagar(double valor, double totalDaCompra) {
+            if (valor < totalDaCompra) return -1;
+            return totalDaCompra - valor;
+        }
+        
+    }
+
+
